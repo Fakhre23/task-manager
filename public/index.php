883eff763f3e1a1aba95c1ddab2a142aa8f51458
Fakhre23 <fakhretamimie@gmail.com>
2025-03-09 12:41:16 +0300

    db connect & structers files

--- public/index.php
+++ public/index.php
@@ -1,16 +1,19 @@
 <?php
 
+require '../includes/db_connect.php';
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
 
 
-
-?>
+?>  
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home Page</title>
 </head>
 <body>
     
