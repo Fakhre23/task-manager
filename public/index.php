1db471aebccf5ea07d5e7fb15bee29e019889217
Fakhre23 <fakhretamimie@gmail.com>
2025-03-11 14:54:49 +0300

    last commit with error in the code

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
 
-require '../includes/db_connect.php';
+include '../includes/db_connect.php';
 
-$sql = "SELECT * FROM users";
-$result = $conn->query($sql);
+$stmt = $conn->prepare("SELECT * FROM users");
+$stmt->execute();
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 ?>  
@@ -14,8 +15,17 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
+    <link rel="stylesheet" href="./css/main.css";>
 </head>
 <body>
-    
+    <h1>Users</h1>
+    <a href="register.php">Add New User</a>
+    <div class="user-container">
+    <?php foreach ($users as $user): ?>
+        <div clas="user-card">
+            <!-- <img src="<?php $users['profile_image'] ?>" alt="User Image"> -->
+             <h2> <?php $users['name'] ?> </h2>
+        </div>
+    </div>
 </body>
 </html>
