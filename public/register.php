3bc3dcd1ff39abac23687fd417b7b5807f5e8483
Fakhre23 <fakhretamimie@gmail.com>
2025-03-20 17:33:23 +0300

    insert profile image from register.php -> Done

--- public/register.php
+++ public/register.php
@@ -1,7 +1,42 @@
 <?php
 
+include '../includes/db_connect.php';
+
+
+if ($_SERVER["REQUEST_METHOD"] =='POST'){
+  $name = $_POST['name'];
+
+
+     /* *****  get user image from uploads->Dir  ******/
+
+$targetDir = "uploads/";
+$fileNme = basename($_FILES["profile-image"]["name"]);
+$targetFilePath = $targetDir . $fileNme;
+$fileType = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
+$allowedTypes = ['jpg' , 'jpeg' , 'png'];
+
+if(in_array($fileType,$allowedTypes)){               // Checking if the File Type is Allowed
+  if(move_uploaded_file($_FILES["profile-image"]["tmp_name"], $targetFilePath)){
+
+    $stmt = $conn->prepare("INSERT INTO users (name , profile_image) VALUES (? , ?);");
+    $stmt->execute([$name , $targetFilePath]);
+    echo "User registered successfully!";
+
+  } else {
+    echo "Error uploading file.";
+  }
+}  else { 
+  echo "Invalid file type. Only JPG, PNG, and GIF allowed.";
+}   
+}
+
+
+
 
 ?>
+
+
+
 
 
 
@@ -11,8 +46,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/main.css";>
 </head>
 <body>
-  <h1>this is register page</h1>  
+  <h1>this is register page</h1> 
+  <a href="index.php">Back</a>
+  <form action="register.php" method="POST" enctype="multipart/form-data" >
+    <input type="text" name="name" placeholder="Enter Name" required>
+    <input type="file" name="profile-image" accept="images/*" required>
+    <button type="submit">Register</button>
+
+  </form>
 </body>
 </html>
